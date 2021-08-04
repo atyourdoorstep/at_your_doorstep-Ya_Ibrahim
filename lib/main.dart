e132f1e892db238925175d8c45c2b46a88fee236
yaxheikh <ibrahimazeem41@gmail.com>
2021-08-04 17:27:26 +0500

    Design changes homepage

--- lib/main.dart
+++ lib/main.dart
@@ -195,7 +195,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => Signup()),
+                                      builder: (context) => HomePage()),
                                 );
                               },
                               color: Colors.white,
