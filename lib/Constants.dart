4e88ddea722e7a16cf95fc8cb9dec0131351e770
yaxheikh <ibrahimazeem41@gmail.com>
2021-08-26 02:09:50 +0500

    default image on null

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -96,7 +96,10 @@ async {
   var res= await CallApi().postData({},'/getProfilePicture' );
   res =json.decode(res.body);
 print(  res.toString());
-  profilePicUrl=res['url'];
+  if(res['url'].toString().length>0)
+  {
+    profilePicUrl=res['url'];
+  }
 return profilePicUrl;
 }
 String profilePicUrl='https://www.pngfind.com/pngs/m/676-6764065_default-profile-picture-transparent-hd-png-download.png';
