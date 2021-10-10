d84058fb221a605dd8f9b35d682a8048754c9279
yaxheikh <ibrahimazeem41@gmail.com>
2021-10-10 16:42:37 +0500

    Working on Order Details page..

--- lib/Screens/Orders/orderDetailPage.dart
+++ lib/Screens/Orders/orderDetailPage.dart
@@ -27,6 +27,7 @@ class _OrderDetailsState extends State<OrderDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -34,20 +35,33 @@ class _OrderDetailsState extends State<OrderDetails> {
           icon: Icon(Icons.arrow_back_ios, color: Colors.red,size: 35,),
         ),
         centerTitle: true,
-        title: Text(orderNo,
-          style: TextStyle(
-              fontSize: 23,
-              color: Colors.red,
-              fontFamily: "PTSans",
-              fontWeight: FontWeight.w700,
-              letterSpacing: 2.0),
-        ),
         backgroundColor: Colors.white,
 
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(orderNo,
+                style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontFamily: "PTSans",
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 2.0),
+              ),
+            ),
+            Text("Ordered Date: ", style:
+            TextStyle(fontSize: 15, color: Colors.black26, fontFamily: "PTSans", fontWeight: FontWeight.w700 )),
+            Text(ordersDetails[0]['created_at'].substring(0,10), style:
+            TextStyle(fontSize: 15, color: Colors.black26, fontFamily: "PTSans", fontWeight: FontWeight.w700 )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Order Details: ", style:
+              TextStyle(fontSize: 15, color: Colors.black26, fontFamily: "PTSans", fontWeight: FontWeight.w700 )),
+            ),
+            SizedBox(height: 10,),
             SizedBox(
               height: ordersDetails.length <= 1 ? 150 :400,
               child: ordersDetails.length >= 1 ? ListView.builder(
@@ -62,31 +76,45 @@ class _OrderDetailsState extends State<OrderDetails> {
                         padding: const EdgeInsets.all(8.0),
                         child: Column(
                           children: [
-                            ListTile(
-                              title: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(ucFirst(ordersDetails[index]['item']['name']),
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                      style: TextStyle(
-                                          color: Colors.black87, fontSize: 15.0, fontWeight: FontWeight.w700),),
-                                  ),
-                                  Padding(
+                            Center(
+                              child: ListTile(
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(ucFirst(ordersDetails[index]['item']['name']),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                        style: TextStyle(
+                                            color: Colors.black87, fontSize: 15.0, fontWeight: FontWeight.w700),),
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                      child: Text("Rs. "+ordersDetails[index]['item']['price'].toString(), style: TextStyle(
+                                        color: Colors.blue,
+                                      ),),
+                                    ),
+                                    Padding(
                                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                                    child: Text("Rs. "+ordersDetails[index]['item']['price'].toString(), style: TextStyle(
-                                      color: Colors.blue,
-                                    ),),
+                                      child: Text("Quantity(s): "+ordersDetails[index]['quantity'].toString(), style: TextStyle(
+                                        color: Colors.blue,
+                                      ),),
+                                    ),
+                                  ],
+                                ),
+                                trailing:  Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Chip(
+                                    shape: StadiumBorder(side: BorderSide(color: Color(0xffD60024), width: 2)),
+                                    label:Text(ucFirst(ordersDetails[index]['status']),
+                                      style: TextStyle(
+                                        color: Color(0xffD60024),fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    backgroundColor: Color(0xFFFFE7E7) ,
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                                    child: Text("Quantity(s): "+ordersDetails[index]['quantity'].toString(), style: TextStyle(
-                                      color: Colors.blue,
-                                    ),),
-                                  ),
-                                ],
+                                ),
                               ),
                             ),
                             Padding(
