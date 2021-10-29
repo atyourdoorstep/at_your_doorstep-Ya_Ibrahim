a89a01d4fd72e51136b6b7985c35d297cb56a8bf
ibrahimTwopointO <worldhappy751@gmail.com>
2021-10-29 12:20:40 +0500

    Setting show seller Profile

--- lib/Screens/SellerControl/showSellerProfile.dart
+++ lib/Screens/SellerControl/showSellerProfile.dart
@@ -7,6 +7,7 @@ import 'package:at_your_doorstep/Screens/Orders/orderDetailPage.dart';
 import 'package:at_your_doorstep/Screens/Orders/sellerOrderDetails.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ShowSellerProfile extends StatefulWidget {
   final sellerID;
@@ -43,109 +44,143 @@ class _ShowSellerProfileState extends State<ShowSellerProfile> {
           icon: Icon(Icons.arrow_back_ios, color: Colors.red,size: 35,),
         ),
       ),
-      body: executed? Container(
-        child: Column(
-          //sellerResult['catItems'].length
-          children: [
-            SizedBox(
-              height: 700,
-              child: ListView.builder(
-                itemCount: sellerResult['catItems'].length,
-                itemBuilder: (context , index){
-                  return
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(ucFirst(sellerResult['catItems'][index]['name']),
-                            style: TextStyle(
-                              color: Color(0xffD60024),fontWeight: FontWeight.w600,
-                              fontSize: 25,
+      body: executed? SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+          CircleAvatar(
+            radius: 60,
+          foregroundImage: NetworkImage(sellerResult['profile']['image'],),
+          child: Text(sellerResult['profile']['id'].toString()),),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(ucFirst(sellerResult['profile']['title']),
+                  style: TextStyle(
+                    color: Colors.black54,fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  //const url = sellerResult['profile']['url'];
+                  if(await canLaunch(sellerResult['profile']['url'])){
+                    await launch(sellerResult['profile']['url']);
+                  }
+                  else{
+                    throw 'Could not launch ${sellerResult['profile']['url']}';
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(ucFirst(sellerResult['profile']['url']),
+                    style: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+
+    SizedBox(
+                height: 700,
+                child: ListView.builder(
+                  itemCount: sellerResult['catItems'].length,
+                  itemBuilder: (context , index){
+                    return
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(ucFirst(sellerResult['catItems'][index]['name']),
+                              style: TextStyle(
+                                color: Color(0xffD60024),fontWeight: FontWeight.w600,
+                                fontSize: 25,
+                              ),
                             ),
                           ),
-                        ),
-                        ////
-                        ListView.builder(
-                          physics: ClampingScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: sellerResult['catItems'][index]['items'].length,
-                          itemBuilder:(context , index1){
-                            return GestureDetector(
-                              onTap: (){
-                                Navigator.push(context, new MaterialPageRoute(
-                                    builder: (context) =>ShowItemPage(itemDetails: sellerResult['catItems'][index]['items'][index1],)));
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Container(
-                                  width: double.infinity,
-                                  height: 120,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey,
-                                        offset: Offset(0.0,1.0),
-                                        blurRadius: 6.0,
+                          ////
+                          ListView.builder(
+                            physics: ClampingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: sellerResult['catItems'][index]['items'].length,
+                            itemBuilder:(context , index1){
+                              return GestureDetector(
+                                onTap: (){
+                                  Navigator.push(context, new MaterialPageRoute(
+                                      builder: (context) =>ShowItemPage(itemDetails: sellerResult['catItems'][index]['items'][index1],)));
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    width: double.infinity,
+                                    height: 120,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey,
+                                          offset: Offset(0.0,1.0),
+                                          blurRadius: 6.0,
+                                        ),
+                                      ],
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10.0),
                                       ),
-                                    ],
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10.0),
+                                      //border: Border.all(color: Colors.red),
                                     ),
-                                    //border: Border.all(color: Colors.red),
-                                  ),
-                                  child: Center(
-                                    child: ListTile(
-                                      leading: ConstrainedBox(
-                                          constraints: BoxConstraints(
-                                            minWidth: 60,
-                                            minHeight: 80,
-                                            maxHeight: 140,
-                                            maxWidth: 120,
+                                    child: Center(
+                                      child: ListTile(
+                                        leading: ConstrainedBox(
+                                            constraints: BoxConstraints(
+                                              minWidth: 60,
+                                              minHeight: 80,
+                                              maxHeight: 140,
+                                              maxWidth: 120,
+                                            ),
+                                            child: Image.network(sellerResult['catItems'][index]['items'][index1]['image'], fit: BoxFit.cover,)),
+                                        title: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['name']),
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 2,
+                                            style: TextStyle(
+                                                color: Colors.black87, fontSize: 15.0, fontWeight: FontWeight.w700),),
+                                        ),
+                                        subtitle: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['description'], ),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                color: Colors.black26, fontSize: 15.0),),
+                                        ),
+                                        trailing: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(10.0),
+                                            ),
+                                            child: Text("Rs. "+sellerResult['catItems'][index]['items'][index1]['price'].toString(), style: TextStyle(
+                                              color: Colors.blue,
+                                            ),),
                                           ),
-                                          child: Image.network(sellerResult['catItems'][index]['items'][index1]['image'], fit: BoxFit.cover,)),
-                                      title: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['name']),
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 2,
-                                          style: TextStyle(
-                                              color: Colors.black87, fontSize: 15.0, fontWeight: FontWeight.w700),),
-                                      ),
-                                      subtitle: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['description'], ),
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                              color: Colors.black26, fontSize: 15.0),),
-                                      ),
-                                      trailing: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(10.0),
-                                          ),
-                                          child: Text("Rs. "+sellerResult['catItems'][index]['items'][index1]['price'].toString(), style: TextStyle(
-                                            color: Colors.blue,
-                                          ),),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            );//categoryItem[index]['image']
-                          },
-                        ),
-                        ////
-                      ],
-                    );
-                },
+                              );//categoryItem[index]['image']
+                            },
+                          ),
+                          ////
+                        ],
+                      );
+                  },
+                ),
               ),
-            ),
-
-
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ): SpecialSpinner(),
     );
@@ -166,78 +201,3 @@ class _ShowSellerProfileState extends State<ShowSellerProfile> {
   }
 
 }
-
-
-// SizedBox(
-//                               height: 500,
-//                               child: ListView.builder(
-//                                 itemCount: sellerResult['catItems'][index]['items'].length,
-//                                 itemBuilder:(context , index1){
-//                                   return GestureDetector(
-//                                     onTap: (){
-//                                       Navigator.push(context, new MaterialPageRoute(
-//                                           builder: (context) =>ShowItemPage(itemDetails: sellerResult['catItems'][index]['items'][index1],)));
-//                                     },
-//                                     child: Padding(
-//                                       padding: const EdgeInsets.all(10.0),
-//                                       child: Container(
-//                                         width: double.infinity,
-//                                         height: 120,
-//                                         decoration: BoxDecoration(
-//                                           color: Colors.white,
-//                                           boxShadow: [
-//                                             BoxShadow(
-//                                               color: Colors.grey,
-//                                               offset: Offset(0.0,1.0),
-//                                               blurRadius: 6.0,
-//                                             ),
-//                                           ],
-//                                           borderRadius: BorderRadius.all(
-//                                             Radius.circular(10.0),
-//                                           ),
-//                                           //border: Border.all(color: Colors.red),
-//                                         ),
-//                                         child: Center(
-//                                           child: ListTile(
-//                                             leading: ConstrainedBox(
-//                                                 constraints: BoxConstraints(
-//                                                   minWidth: 60,
-//                                                   minHeight: 80,
-//                                                   maxHeight: 140,
-//                                                   maxWidth: 120,
-//                                                 ),
-//                                                 child: Image.network(sellerResult['catItems'][index]['items'][index1]['image'], fit: BoxFit.cover,)),
-//                                             title: Padding(
-//                                               padding: const EdgeInsets.all(8.0),
-//                                               child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['name']),
-//                                                 overflow: TextOverflow.ellipsis,
-//                                                 maxLines: 2,
-//                                                 style: TextStyle(
-//                                                     color: Colors.black87, fontSize: 15.0, fontWeight: FontWeight.w700),),
-//                                             ),
-//                                             subtitle: Padding(
-//                                               padding: const EdgeInsets.all(8.0),
-//                                               child: Text(ucFirst(sellerResult['catItems'][index]['items'][index1]['description'], ),
-//                                                 overflow: TextOverflow.ellipsis,
-//                                                 style: TextStyle(
-//                                                     color: Colors.black26, fontSize: 15.0),),
-//                                             ),
-//                                             trailing: Padding(
-//                                               padding: const EdgeInsets.all(8.0),
-//                                               child: ClipRRect(
-//                                                 borderRadius: BorderRadius.all(
-//                                                   Radius.circular(10.0),
-//                                                 ),
-//                                                 child: Text("Rs. "+sellerResult['catItems'][index]['items'][index1]['price'].toString(), style: TextStyle(
-//                                                   color: Colors.blue,
-//                                                 ),),
-//                                               ),
-//                                             ),
-//                                           ),
-//                                         ),
-//                                       ),
-//                                     ),
-//                                   );//categoryItem[index]['image']
-//                                 },
-//                               ),
-//                             ),
