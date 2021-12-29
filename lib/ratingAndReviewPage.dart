f6b2fb3c51132c0e581978136df05d6e31d81739
yaxheikh <ibrahimazeem41@gmail.com>
2021-12-29 20:18:41 +0500

    Rating and Reviews UI

--- lib/ratingAndReviewPage.dart
+++ lib/ratingAndReviewPage.dart
@@ -14,6 +14,8 @@ class RatingAndReview extends StatefulWidget {
 }
 
 class _RatingAndReviewState extends State<RatingAndReview> {
+
+  int rating = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,14 +37,31 @@ class _RatingAndReviewState extends State<RatingAndReview> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(width: 10,),
-                    Icon(Icons.star_border, size: 50,),
-                  ],
+
+                    GestureDetector(onTap:(){setState(() {
+                      rating=1;
+                    });},child: rating >=1?Icon(Icons.star, size: 50,color: Colors.yellow,):Icon(Icons.star_border, size: 50,color: Colors.black12,)),
+                    GestureDetector(onTap:(){setState(() {
+                      rating=2;
+                    });},child: rating >=2?Icon(Icons.star, size: 50,color: Colors.yellow,) :Icon(Icons.star_border, size: 50,color: Colors.black12,)),
+                    GestureDetector(onTap:(){setState(() {
+                      rating=3;
+                    });},child: rating >=3?Icon(Icons.star, size: 50,color: Colors.yellow,) :Icon(Icons.star_border, size: 50,color: Colors.black12,)),
+                    GestureDetector(onTap:(){setState(() {
+                      rating=4;
+                    });},child: rating >=4?Icon(Icons.star, size: 50,color: Colors.yellow,) :Icon(Icons.star_border, size: 50,color: Colors.black12,)),
+                    GestureDetector(onTap:(){setState(() {
+                      rating=5;
+                    });},child: rating >=5?Icon(Icons.star, size: 50,color: Colors.yellow,) :Icon(Icons.star_border, size: 50,color: Colors.black12,)),
+                   ],
                 ),
               ),
+              SizedBox(height: 10,),
               Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Row(
                   children: [
                     Expanded(
